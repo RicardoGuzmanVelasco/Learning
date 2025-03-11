4fe078896cc82e216c71274490b0b2bc5698cee9
RGV <ricardo.guzman.velasco@outlook.com>
2025-03-11 12:06:30 +0100

    feat: water a plant

---
+++ Plants/Pot.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Plants
+{
+    public class Pot
+    {
+        string plantId;
+        
+        public bool IsEmpty => string.IsNullOrEmpty(plantId);
+        public bool IsWet { get; private set; }
+        
+        public void Water()
+        {
+            Debug.Assert(!IsEmpty);
+            Debug.Assert(!IsWet);
+            
+            IsWet = true;
+        }
+
+        public void ArrancarPlant()
+        {
+            Debug.Assert(!IsEmpty);
+            
+            plantId = null;
+        }
+    }
+}
---
+++ PlantsTests/PotTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Plants
+{
+    public class PotTests
+    {
+        [Test]
+        public void NewPotIsEmpty()
+        {
+            Assert.That(new Pot().IsEmpty, Is.True);
+        }
+
+        [Test]
+        public void NewPotIsNotWet()
+        {
+            Assert.That(new Pot().IsWet, Is.False);
+        }
+    }
+}
